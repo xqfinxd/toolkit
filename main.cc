1893717e069d4b3fba850f9a967478d7662b32ae
xqfinxd <2625513819@qq.com>
2022-03-07 12:04:23 +0800

    test commit

--- main.cc
+++ main.cc
@@ -1,26 +1,6 @@
 #include <iostream>
 #include <vector>
 
-struct Element {
-	std::string hello;
-	Element() {}
-};
-
-using Vector = std::vector<Element>;
-
-static Vector s_vec = {
-	Element(), Element(), Element(), Element(),
-};
-
-const Vector GetVector() {
-	return s_vec;
-}
-
 int main() {
-	{
-		std::cout << s_vec.size() << std::endl;
-		auto vec = GetVector();
-		std::cout << vec.size() << std::endl;
-	}
-	std::cout << s_vec.size() << std::endl;
+	std::cout << "hello world" << std::endl;
 }
